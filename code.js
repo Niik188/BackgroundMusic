1c22372d7387e4c0779b8830b395a2c6866e0aa6
Niik18 <ermolin.nik12@mail.com>
2023-06-17 12:33:12 +0300

    test

---
+++ code.js
@@ -0,0 +1,20 @@
+navigator.geolocation.getCurrentPosition(success, error, {
+    // высокая точность
+    enableHighAccuracy: true
+  })
+  
+  function success({ coords }) {
+    // получаем широту и долготу
+    const { latitude, longitude } = coords
+    const position = [latitude, longitude]
+     // [широта, долгота]
+     setInterval(() => {
+    console.log(position)
+    }, 1000);
+  }
+  
+  function error({ message }) {
+    console.log(message) // при отказе в доступе получаем PositionError: User denied Geolocation
+  }
+
+  
